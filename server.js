01b794fc87bf9104b33163d95afd313a493bda1b
John Gallagher <gallagherj0913@gmail.com>
2021-11-07 23:48:34 -0700

    losing the if statement

--- server.js
+++ server.js
@@ -63,7 +63,7 @@ app.post('/api/notes', (req, res) => {
 
   const { title, text } = req.body;
 
-  if (title && text) {
+
     const newNote = {
       title,
       text,
@@ -72,7 +72,7 @@ app.post('/api/notes', (req, res) => {
 
     readAndAppend(newNote, './db/db.json');
     res.json(newNote);
-  }
+
 });
 // Bonus: DELETE /api/notes/:id should receive a query parameter that contains the id of a note to delete. To delete a note, you'll need to read all notes from the db.json file, remove the note with the given id property, and then rewrite the notes to the db.json file.
 
