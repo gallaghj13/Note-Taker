e8979550a7f8f9a37f2edb21d06df0164e55a164
John Gallagher <gallagherj0913@gmail.com>
2021-11-07 22:23:30 -0700

    added post method and the functions to read and write files

--- server.js
+++ server.js
@@ -4,8 +4,9 @@ const fs = require('fs');
 const util = require('util');
 // var uniqid = require('uniqid'); 
 // const api = require('./db/db.json');
+const uuid = require('./helpers/uuid'); 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 // GET /notes should return the notes.html file.
@@ -30,12 +31,51 @@ const readFromFile = util.promisify(fs.readFile);
  *  @param {object} content The content you want to write to the file.
  *  @returns {void} Nothing
  */
+ const writeToFile = (destination, content) =>
+ fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
+   err ? console.error(err) : console.info(`\nData written to ${destination}`)
+ );
+
+ /**
+ *  Function to read data from a given a file and append some content
+ *  @param {object} content The content you want to append to the file.
+ *  @param {string} file The path to the file you want to save to.
+ *  @returns {void} Nothing
+ */
+ const readAndAppend = (content, file) => {
+  fs.readFile(file, 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+      const parsedData = JSON.parse(data);
+      parsedData.push(content);
+      writeToFile(file, parsedData);
+    }
+  });
+};
 
 app.get('/api/notes', (req, res) =>
   readFromFile('./db.json').then((data) => res.json(JSON.parse(data)))
 );
 // POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
+app.post('/api/notes', (req, res) => {
+  console.info(`${req.method} request received to add a note`);
 
+  const { title, text } = req.body;
+
+  if (req.body) {
+    const newNote = {
+      title,
+      text,
+      note_id: uuid(),
+    };
+
+    readAndAppend(newNote, './db/db.json');
+    res.json(`Note added successfully`);
+  } else {
+    res.error('Error in adding note');
+  }
+});
 // Bonus: DELETE /api/notes/:id should receive a query parameter that contains the id of a note to delete. To delete a note, you'll need to read all notes from the db.json file, remove the note with the given id property, and then rewrite the notes to the db.json file.
 
 
