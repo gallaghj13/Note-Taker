040fea0685ffcf2c921de7ad0906abee87fd4d90
John Gallagher <gallagherj0913@gmail.com>
2021-11-08 13:05:30 -0700

    changes

--- server.js
+++ server.js
@@ -55,7 +55,7 @@ const readFromFile = util.promisify(fs.readFile);
 };
 
 app.get('/api/notes', (req, res) =>
-  readFromFile('/Users/johngallagher/Note-Taker/db/db.json').then((data) => res.json(JSON.parse(data)))
+  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
 );
 // POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
 app.post('/api/notes', (req, res) => {
@@ -64,16 +64,16 @@ app.post('/api/notes', (req, res) => {
   const { title, text } = req.body;
 
 
-  if (req.body) {
+  // if (req.body) {
     const newNote = {
       title,
       text,
       tip_id: uniqid(),
     };
 
-    readAndAppend(newNote, '/Users/johngallagher/Note-Taker/db/db.json');
+    readAndAppend(newNote, './db/db.json');
     res.json(newNote);
-  }
+  // }
 });
 // Bonus: DELETE /api/notes/:id should receive a query parameter that contains the id of a note to delete. To delete a note, you'll need to read all notes from the db.json file, remove the note with the given id property, and then rewrite the notes to the db.json file.
 
